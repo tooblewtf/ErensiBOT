ef7952b684531e6023339e7df018c242aa6cb60e
tooble <guguli063@gmail.com>
2025-06-28 18:22:11 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    token: "BOT_TOKEN_BURAYA", 
+    prefix: "e!",
+    botdavet: "BOT_DAVET_BURAYA",
+    desteksunucusu: "BOT_DESTEK_SUNUCUSU_BURAYA",
+    website: "BOT_WEBSITE_BURAYA",
+    topgg: "BOT_TOPGG_KEY_BURAYA",
+    politika: "BOT_TOS_BURAYA",
+    sunucuid: "BOT_DESTEKSUNUCUID_BURAYA",
+    destekçi: "BOT_DESTEKCI_BURAYA"
+}
